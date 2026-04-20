d5040184407302fb083dc57473d70678596f4f1a
IgoorToorres <igorevangelista121@gmail.com>
2026-04-20 17:24:10 -0300

    fix - link formulario ajustado

--- src/app/formulario/page.tsx
+++ src/app/formulario/page.tsx
@@ -1,4 +1,15 @@
+import type { Metadata } from 'next';
 import { PostForm } from '@/components/formulario';
+
+export const metadata: Metadata = {
+  title: 'Deixe sua mensagem para o casal',
+  description: 'Registre sua recordação especial do nosso casamento ❤️',
+  openGraph: {
+    title: 'Deixe sua mensagem para o casal',
+    description: 'Registre sua recordação especial do nosso casamento ❤️',
+    images: ['/Natan-Bia.jpeg'],
+  },
+};
 
 export default function Formulario() {
   return <PostForm />;
